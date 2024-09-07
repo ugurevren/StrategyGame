9bf595045b6eaedd0cb0697204094d9c87faeee4
Uğur Evren Çamalan <ugurevrencamalan@gmail.com>
2024-09-07 17:33:31 +0300

    Completed Generic Grid System

--- Assets/Scripts/GridSystem/Grid.cs
+++ Assets/Scripts/GridSystem/Grid.cs
@@ -1,16 +1,25 @@
+using System;
 using UnityEngine;
 
 namespace GridSystem
 {
     public class Grid<TGridObject>
     {
+        public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+        public class OnGridObjectChangedEventArgs : EventArgs
+        {
+            public int x;
+            public int y;
+        }
+
         private int _width;
         private int _height;
         private float _cellSize;
         private Vector3 _originPosition;
         private TGridObject[,] _gridArray;
         private TextMesh[,] _debugTextArray;
-        public Grid(int width, int height, float cellSize, Vector3 originPosition)
+        public Grid(int width, int height, float cellSize, Vector3 originPosition,
+            Func<Grid<TGridObject>,int,int,TGridObject> createGridObject, bool showDebug = false)
         {
             _width = width;
             _height = height;
@@ -18,40 +27,87 @@ namespace GridSystem
             _originPosition = originPosition;
         
             _gridArray = new TGridObject[width, height];
-            _debugTextArray = new TextMesh[width, height];
+            
         
             for(int x=0;x < _gridArray.GetLength(0); x++)
             {
-                for(int y=0; y < _gridArray.GetLength(1); y++)
+                for (int y = 0; y < _gridArray.GetLength(1); y++)
                 {
-                    _debugTextArray[x,y]=CreateWorldText(_gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + 
-                        new Vector3(_cellSize, _cellSize) * 0.5f, 20, Color.white);
-                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
-                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                    _gridArray[x, y] = createGridObject(this, x, y);
+                    
                 }
             }
-            Debug.DrawLine(GetWorldPosition(0, _height), GetWorldPosition(_width, _height), Color.white, 100f);
-            Debug.DrawLine(GetWorldPosition(_width, 0), GetWorldPosition(_width, _height), Color.white, 100f);
+
+            if (showDebug)
+            {
+                _debugTextArray = new TextMesh[width, height];
+                for (int x = 0; x < _gridArray.GetLength(0); x++)
+                {
+                    for (int y = 0; y < _gridArray.GetLength(1); y++)
+                    {
+                        _debugTextArray[x, y] = CreateWorldText(_gridArray[x, y]?.ToString(), null, GetWorldPosition(x, y) + new Vector3(_cellSize, _cellSize) * 0.5f, 30, Color.white, TextAnchor.MiddleCenter, TextAlignment.Center);
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                    }
+                }
+                Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+                
+            }
         }
         
-        private void SetValue(int x, int y, TGridObject value)
+        public void TriggerGridObjectChanged(int x, int y)
+        {
+            if(OnGridObjectChanged!= null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs {x = x, y = y});
+        }
+        
+        private void SetGridObject(int x, int y, TGridObject value)
         {
             if(x >= 0 && y >= 0 && x < _width && y < _height)
             {
                 _gridArray[x, y] = value;
-                _debugTextArray[x, y].text = _gridArray[x, y].ToString();
+                if(OnGridObjectChanged!= null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs {x = x, y = y});
             }
         }
-        public void SetValue(Vector3 worldPosition, TGridObject value)
+        public void SetGridObject(Vector3 worldPosition, TGridObject value)
         {
             int x, y;
             GetXY(worldPosition, out x, out y);
-            SetValue(x, y, value);
+            SetGridObject(x, y, value);
         }
+        
+        public Vector3 GetWorldPosition(int x, int y)
+        {
+            return new Vector3(x, y) * _cellSize + _originPosition;
+        }
+        public TGridObject GetGridObject(int x, int y)
+        {
+            if(x >= 0 && y >= 0 && x < _width && y < _height)
+            {
+                return _gridArray[x, y];
+            }
+            return default;
+        }
+        
+        public TGridObject GetGridObject(Vector3 worldPosition)
+        {
+            int x, y;
+            GetXY(worldPosition, out x, out y);
+            return GetGridObject(x, y);
+        }
+        
         private void GetXY(Vector3 worldPosition, out int x, out int y)
         {
             x = Mathf.FloorToInt((worldPosition-_originPosition).x / _cellSize);
             y = Mathf.FloorToInt((worldPosition-_originPosition).y / _cellSize);
+        }
+        public int GetWidth()
+        {
+            return _width;
+        }
+        public int GetHeight()
+        {
+            return _height;
         }
     
         private static TextMesh CreateWorldText(string text, Transform parent = null, 
@@ -73,24 +129,6 @@ namespace GridSystem
             textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
             return textMesh;
         }
-        private Vector3 GetWorldPosition(int x, int y)
-        {
-            return new Vector3(x, y) * _cellSize + _originPosition;
-        }
-        public TGridObject GetValue(int x, int y)
-        {
-            if(x >= 0 && y >= 0 && x < _width && y < _height)
-            {
-                return _gridArray[x, y];
-            }
-            return default;
-        }
         
-        public TGridObject GetValue(Vector3 worldPosition)
-        {
-            int x, y;
-            GetXY(worldPosition, out x, out y);
-            return GetValue(x, y);
-        }
     }
 }
--- Assets/Scripts/GridSystem/GridTester.cs
+++ Assets/Scripts/GridSystem/GridTester.cs
@@ -1,35 +1,110 @@
-using GridSystem;
 using UnityEngine;
 
-
-public class GridTester : MonoBehaviour
+namespace GridSystem
 {
-    [SerializeField] private int _width;
-    [SerializeField] private int _height;
-    [SerializeField] private float _cellSize;
-    [SerializeField] private Vector3 _originPosition;
-    private Grid<bool> _grid;
+    public class GridTester : MonoBehaviour
+    {
+        [SerializeField] public int _width;
+        [SerializeField] private int _height;
+        [SerializeField] private float _cellSize;
+        [SerializeField] private Vector3 _originPosition;
+        private Grid<GridObject> _grid;
     
-    private void Start()
-    {
-         _grid = new Grid<bool>(_width, _height, _cellSize, _originPosition);
-    }
+        private void Start()
+        {
+            _grid = new Grid<GridObject>(_width, _height, _cellSize, _originPosition,
+                (Grid<GridObject> g,int x, int y) => new GridObject(g,x,y),true);
+        }
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
+        private void Update()
         {
-            _grid.SetValue(GetMouseWorldPosition(), true);
+            if (Input.GetMouseButtonDown(0))
+            {
+                GridObject gridObject = _grid.GetGridObject(GetMouseWorldPosition());
+                if (gridObject != null)
+                {
+                    Debug.Log("Mouse position: " + GetMouseWorldPosition() + " GridObject: " + gridObject.X + "," + gridObject.Y);
+                }
+            }
         }
-        if(Input.GetMouseButtonDown(1))
+        public Vector3 GetMouseWorldPosition()
         {
-            Debug.Log(_grid.GetValue(GetMouseWorldPosition()));
+            var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0;
+            return worldPosition;
+        }
+        public void SetGridType(Vector3 worldPosition, GridObject.GridType type, int index)
+        {
+            _grid.GetGridObject(worldPosition).Set(type, index);
+        }
+        public GridObject GetGridObject (int x, int y)
+        {
+            return _grid.GetGridObject(x, y);
+        }
+        
+        public GridObject GetGridObject (Vector3 worldPosition)
+        {
+            return _grid.GetGridObject(worldPosition);
+        }
+
+        public void Clear()
+        {
+            if(_grid == null) return;
+            // Clear the grid
+            for (var x = 0; x < _grid.GetWidth(); x++)
+            {
+                for (var y = 0; y < _grid.GetHeight(); y++)
+                {
+                    _grid.GetGridObject(x, y).Set(GridObject.GridType.Empty, 0);
+                }
+            }
+        }
+       
+    }
+    
+    public class GridObject
+    {
+        [System.Serializable]
+        public enum GridType
+        {
+            Building,
+            Enemy,
+            FriendlyUnit,
+            Empty
+            
+        }
+        public Grid<GridObject> grid;
+        public GridType Type { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int index;
+        public int rotation;
+        
+        public GridObject(Grid<GridObject> grid, int x, int y, int rotation = 0)
+        {
+            this.grid = grid;
+            this.X = x;
+            this.Y = y;
+            this.Type = GridType.Empty;
+            this.rotation = rotation;
+        }
+        
+        public void Set(GridType type, int index)
+        {
+            this.Type = type;
+            this.index = index;
+            grid.TriggerGridObjectChanged(X,Y);
+        }
+        
+        public override string ToString()
+        {
+            return Type.ToString()+index;
+        }
+        
+        public Vector3 GetWorldPosition()
+        {
+            return grid.GetWorldPosition(X, Y);
         }
     }
-    public Vector3 GetMouseWorldPosition()
-    {
-        var worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        worldPosition.z = 0;
-        return worldPosition;
-    }
+    
 }
